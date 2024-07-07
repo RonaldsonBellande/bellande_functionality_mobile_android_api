e68963bc221965940c8350c573c8b03e937089e0
RonaldsonBellande <ronaldsonbellande@gmail.com>
2024-07-07 14:52:20 -0400

    latest pushes

---
+++ bellande_computer_vision_2d/src/main/java/com/bellande_api/bellande_computer_vision_2d/bellande_computer_vision_2d_prediction_api.java
@@ -0,0 +1,45 @@
+
+/**
+ * Copyright (C) 2024 Bellande Application UI UX Research Innovation Center, Ronaldson Bellande
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **/
+
+package com.bellande_api.bellande_computer_vision_2d_prediction;
+
+import com.google.gson.annotations.SerializedName;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface Bellande_Computer_Vision_2D_Prediction_Api {
+    @POST("prediction")
+    Call<BellandeResponse> getBellandeResponse(@Body RequestBody requestBody);
+
+    class RequestBody {
+        @SuppressWarnings("unused")
+        @SerializedName("input_text")
+        private final String inputText;
+
+        public RequestBody(String inputText) {
+            this.inputText = inputText;
+        }
+    }
+
+    class BellandeResponse {
+        @SerializedName("response")
+        public String response;
+    }
+}
